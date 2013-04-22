6459f2009fb960f75b707b6f84da12cb298c88ef
Pal David Gergely - night[w] <nightw17@gmail.com>
2013-04-22 13:41:25 +0200

    RDOC compatible license and copyright information added. Also a bit of explanation of the planned implementation.

--- nightw_competitor.rb
+++ nightw_competitor.rb
@@ -1,5 +1,25 @@
 #!/usr/bin/env ruby
 
+# The program is written for the scaling contest of Prezi.com which can
+# be found here: http://prezi.com/scale/
+#
+# The basic concept of the algorithm is the following:
+# * There are always a (comaparatively low) fixed number of virtual
+#   machines available as an idle pool to process the incoming
+#   requests in the near future
+# * There is also a simple trend analizer for a time windows of the past
+#   hour to try to guess the amount of VMs needed for the 60 minutes
+# * The pool of running VMs is consisted of sum of the above constant
+# number and the predicted need from the trend of last hour
+#
+# Or the shortest summary is that the program tries to implement
+# a classic PD controller which tries to use the queues input to
+# control the number of VMs used.
+
+# Author::    Pal David Gergely  (mailto:nightw17@gmail.com)
+# Copyright:: Copyright (c) 2013 Pal David Gergely
+# License::   Apache License, Version 2.0
+
 # We check the Ruby version and print a warning if it's not 1.9.x
 if !RUBY_VERSION.start_with?('1.9')
 	$stderr.puts 'WARNING: The code was tested only on Ruby 1.9!!!' 
